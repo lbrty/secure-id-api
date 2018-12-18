333063626e8c8f28f53d9c00ccb5fb5dc5125d43
Sultan Iman <sultan.imanhodjaev@gmail.com>
2018-12-19 00:07:39 +0100

    refactor: move user schema from auth into core

---
+++ apps/core/lib/core/user/user.ex
@@ -0,0 +1,23 @@
+defmodule Core.User do
+  use Ecto.Schema
+
+  @timestamp_opts [type: :utc_datetime]
+  schema "users" do
+    field :last_name, :string
+    field :first_name, :string
+    field :email, :string
+
+    # passwords
+    field :password_hash, :string
+    field :password, :string, virtual: true
+
+    # states
+    field :is_active, :boolean, default: true
+    field :is_admin, :boolean, default: false
+
+    # Related permissions
+    has_many :permissions, Core.Permission
+
+    timestamps()
+  end
+end
