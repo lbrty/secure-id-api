4fa7801738b2fcb831d46855052d5ffc29687ba6
Sultan Iman <sultan.imanhodjaev@gmail.com>
2018-12-21 21:58:54 +0100

    chore: create join table for idps and tags

---
+++ apps/core/lib/core/idp/idps_tags.ex
@@ -0,0 +1,14 @@
+defmodule Core.IdpsTags do
+  @moduledoc false
+  use Core.Schema
+
+  # This is just a join table for m2m
+  # relationship between Idps and tags.
+  @primary_key false
+  schema "idps_tags" do
+    belongs_to(:idp, Core.Idp)
+    belongs_to(:tag, Core.Tag)
+
+    timestamps()
+  end
+end
