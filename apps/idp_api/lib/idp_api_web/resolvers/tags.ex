b4fb1fad04a062ec1197f811b07330e095d08909
Sultan Iman <sultan.imanhodjaev@gmail.com>
2018-12-24 11:40:05 +0100

    chore: create tag resolvers

---
+++ apps/idp_api/lib/idp_api_web/resolvers/tags.ex
@@ -0,0 +1,21 @@
+defmodule IdpApiWeb.Resolvers.Tags do
+  alias Core.Repo
+  alias Core.Tag
+  alias Core.TagActions
+
+  def list(_parent, _args, _resolution) do
+    {:ok, Tag |> Repo.all}
+  end
+
+  def create_tag(_parent, args, _resolution) do
+    {:ok, TagActions.create(args)}
+  end
+
+  def update_tag(_parent, args, _resolution) do
+    TagActions.update(args)
+  end
+
+  def delete_tag(_parent, args, _resolution) do
+    TagActions.delete(args)
+  end
+end
