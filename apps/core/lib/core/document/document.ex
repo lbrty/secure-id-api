67b78bb08d42fd5cdb447661429a2a5bd2e829a9
Sultan Iman <sultan.imanhodjaev@gmail.com>
2018-12-24 20:14:03 +0100

    chore: define default changesets for document schema

--- apps/core/lib/core/document/document.ex
+++ apps/core/lib/core/document/document.ex
@@ -1,6 +1,9 @@
 defmodule Core.Document do
   use Core.Model
   use Arc.Ecto.Schema
+  import Ecto.Changeset
+
+  @required_fields [:original_name, :document, :idp]
 
   schema "documents" do
     field(:external_id, Ecto.UUID, autogenerate: true)
@@ -10,5 +13,17 @@ defmodule Core.Document do
     belongs_to(:idp, Core.Idp)
 
     timestamps()
+  end
+
+  @doc false
+  def changeset(params) do
+    %Core.Document{} |> changeset(params)
+  end
+
+  @doc false
+  def changeset(%Core.Document{} = document, params) do
+    document
+    |> cast(params, @required_fields)
+    |> validate_required(@required_fields)
   end
 end
