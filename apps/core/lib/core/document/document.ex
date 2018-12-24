bc5354d99b9587b16345313f82618b5715cee3fb
Sultan Iman <sultan.imanhodjaev@gmail.com>
2018-12-24 19:41:49 +0100

    refactor: rename :file field to :document and use arc uploader

--- apps/core/lib/core/document/document.ex
+++ apps/core/lib/core/document/document.ex
@@ -1,10 +1,11 @@
 defmodule Core.Document do
   use Core.Model
+  use Arc.Ecto.Schema
 
   schema "documents" do
     field(:external_id, Ecto.UUID, autogenerate: true)
     field(:original_name, :string)
-    field(:file, :string)
+    field(:document, Core.DocumentUploader.Type)
 
     belongs_to(:idp, Core.Idp)
 
