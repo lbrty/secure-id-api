2a67c8e17969c7cacebec1a503dd9382c3741dd6
Sultan Iman <sultan.imanhodjaev@gmail.com>
2018-12-24 22:52:03 +0100

    refactor: optimize aliases for project queries

--- apps/idp_api/lib/idp_api_web/schema/project/project_queries.ex
+++ apps/idp_api/lib/idp_api_web/schema/project/project_queries.ex
@@ -1,11 +1,11 @@
 defmodule IdpApiWeb.Schema.ProjectQueries do
   use Absinthe.Schema.Notation
-  alias IdpApiWeb.Resolvers
+  alias IdpApiWeb.Resolvers.Projects
 
   object :project_queries do
     @desc "Get all projects"
     field :projects, list_of(:project) do
-      resolve &Resolvers.Projects.list/3
+      resolve &Projects.list/3
     end
   end
 end
