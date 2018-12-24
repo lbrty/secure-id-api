797303e87c91ba7cc278fd18aef01459097cf075
Sultan Iman <sultan.imanhodjaev@gmail.com>
2018-12-24 22:35:56 +0100

    chore: create city schema mutations

--- apps/idp_api/lib/idp_api_web/schema/world/world_mutations.ex
+++ apps/idp_api/lib/idp_api_web/schema/world/world_mutations.ex
@@ -3,6 +3,7 @@ defmodule IdpApiWeb.Schema.WorldMutations do
   alias IdpApiWeb.Resolvers.World
 
   object :world_mutations do
+    # Country mutations
     @desc "Create country"
     field :create_country, :country do
       arg :title, non_null(:string)
@@ -25,6 +26,7 @@ defmodule IdpApiWeb.Schema.WorldMutations do
       resolve &World.delete_country/3
     end
 
+    # State mutations
     @desc "Create state"
     field :create_state, :state do
       arg :title, non_null(:string)
@@ -46,6 +48,30 @@ defmodule IdpApiWeb.Schema.WorldMutations do
       arg :id, non_null(:integer)
 
       resolve &World.delete_state/3
+    end
+
+    # City mutations
+    @desc "Create city"
+    field :create_city, :city do
+      arg :title, non_null(:string)
+      arg :state_id, non_null(:integer)
+
+      resolve &World.create_city/3
+    end
+
+    @desc "Update city"
+    field :update_city, type: :city do
+      arg :id, non_null(:integer)
+      arg :city, :update_city_params
+
+      resolve &World.update_city/3
+    end
+
+    @desc "Delete city"
+    field :delete_city, type: :city do
+      arg :id, non_null(:integer)
+
+      resolve &World.delete_city/3
     end
   end
 end
