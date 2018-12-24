a929eada44d526c5cb030e17ca030c2156beebd1
Sultan Iman <sultan.imanhodjaev@gmail.com>
2018-12-24 23:12:29 +0100

    fix: fix typo in constraint validation field

--- apps/core/lib/core/world/state.ex
+++ apps/core/lib/core/world/state.ex
@@ -23,6 +23,6 @@ defmodule Core.State do
     state
     |> cast(params, @required_fields)
     |> validate_required(@required_fields)
-    |> foreign_key_constraint(:state_id)
+    |> foreign_key_constraint(:country_id)
   end
 end
