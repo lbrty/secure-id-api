f732f9126ffedd9cefcf21acaf8f4021c17d5f3c
Sultan Iman <sultan.imanhodjaev@gmail.com>
2018-12-24 11:03:25 +0100

    chore: add default changesets and actions for tags schema

--- apps/core/lib/core/tag/tag.ex
+++ apps/core/lib/core/tag/tag.ex
@@ -1,8 +1,21 @@
 defmodule Core.Tag do
   @moduledoc false
   use Core.Schema
+  import Ecto.Changeset
 
   schema "tags" do
     field(:name, :string)
+  end
+
+  @doc false
+  def changeset(params) do
+    %Core.Tag{} |> changeset(params)
+  end
+
+  @doc false
+  def changeset(%Core.Tag{} = tag, params) do
+    tag
+    |> cast(params, [:title])
+    |> validate_required([:title])
   end
 end
---
+++ apps/core/lib/core/tag/tag_actions.ex
@@ -0,0 +1,23 @@
+defmodule Core.TagActions do
+  alias Core.Repo
+  alias Core.Tag
+
+  def create(params) do
+    params
+    |> Tag.changeset
+    |> Repo.insert!
+  end
+
+  def update(%{id: id, tag: params}) do
+    Tag
+    |> Repo.get!(id)
+    |> Tag.changeset(params)
+    |> Repo.update
+  end
+
+  def delete(%{id: id}) do
+    Tag
+    |> Repo.get!(id)
+    |> Repo.delete
+  end
+end
