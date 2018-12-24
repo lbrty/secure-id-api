0587b839d79f2cf190d4bc25e3e8fe30aec5bad1
Sultan Iman <sultan.imanhodjaev@gmail.com>
2018-12-24 22:51:51 +0100

    refactor: optimize aliases for project mutations

--- apps/idp_api/lib/idp_api_web/schema/project/project_mutations.ex
+++ apps/idp_api/lib/idp_api_web/schema/project/project_mutations.ex
@@ -1,6 +1,6 @@
 defmodule IdpApiWeb.Schema.ProjectMutations do
   use Absinthe.Schema.Notation
-  alias IdpApiWeb.Resolvers
+  alias IdpApiWeb.Resolvers.Projects
 
   object :project_mutations do
     @desc "Create project"
@@ -8,7 +8,7 @@ defmodule IdpApiWeb.Schema.ProjectMutations do
       arg :project_name, non_null(:string)
       arg :description, non_null(:string)
 
-      resolve &Resolvers.Projects.create_project/3
+      resolve &Projects.create_project/3
     end
 
     @desc "Update project"
@@ -16,14 +16,14 @@ defmodule IdpApiWeb.Schema.ProjectMutations do
       arg :id, non_null(:integer)
       arg :project, :update_project_params
 
-      resolve &Resolvers.Projects.update_project/3
+      resolve &Projects.update_project/3
     end
 
     @desc "Delete project"
     field :delete_project, type: :project do
       arg :id, non_null(:integer)
 
-      resolve &Resolvers.Projects.delete_project/3
+      resolve &Projects.delete_project/3
     end
   end
 end
