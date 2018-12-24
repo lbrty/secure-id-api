bce3cd1ffabec5465b287b44e6a82e80967018c1
Sultan Iman <sultan.imanhodjaev@gmail.com>
2018-12-24 22:23:12 +0100

    chore: add state types

--- apps/idp_api/lib/idp_api_web/schema/world/world_types.ex
+++ apps/idp_api/lib/idp_api_web/schema/world/world_types.ex
@@ -1,13 +1,27 @@
 defmodule IdpApiWeb.Schema.WorldTypes do
   use Absinthe.Schema.Notation
+  use Absinthe.Ecto, repo: Core.Repo
 
   input_object :update_country_params do
     field :title, :string
+  end
+
+  input_object :update_state_params do
+    field :title, :string
+    field :country_id, :integer
   end
 
   @desc "Country"
   object :country do
     field :id, :id
     field :title, :string
+    field :states, list_of(:state), resolve: assoc(:states)
+  end
+
+  @desc "State"
+  object :state do
+    field :id, :id
+    field :title, :string
+    field :country, :country, resolve: assoc(:country)
   end
 end
