fe3c4620788600893c8cd0be2a3309f5c0189540
Sultan Iman <sultan.imanhodjaev@gmail.com>
2018-12-24 22:22:46 +0100

    chore: implement state mutations

--- apps/idp_api/lib/idp_api_web/schema/world/world_mutations.ex
+++ apps/idp_api/lib/idp_api_web/schema/world/world_mutations.ex
@@ -24,5 +24,28 @@ defmodule IdpApiWeb.Schema.WorldMutations do
 
       resolve &World.delete_country/3
     end
+
+    @desc "Create state"
+    field :create_state, :state do
+      arg :title, non_null(:string)
+      arg :country_id, non_null(:integer)
+
+      resolve &World.create_state/3
+    end
+
+    @desc "Update state"
+    field :update_state, type: :state do
+      arg :id, non_null(:integer)
+      arg :state, :update_state_params
+
+      resolve &World.update_state/3
+    end
+
+    @desc "Delete state"
+    field :delete_state, type: :state do
+      arg :id, non_null(:integer)
+
+      resolve &World.delete_state/3
+    end
   end
 end
