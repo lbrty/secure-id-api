f7d05b278471145b375b95371f6bbec0cac0261d
Sultan Iman <sultan.imanhodjaev@gmail.com>
2018-12-24 22:22:30 +0100

    chore: implement state resolvers

--- apps/idp_api/lib/idp_api_web/resolvers/world.ex
+++ apps/idp_api/lib/idp_api_web/resolvers/world.ex
@@ -1,8 +1,10 @@
 defmodule IdpApiWeb.Resolvers.World do
   alias Core.Repo
-  alias Core.Country
+  alias Core.{Country, State}
   alias Core.WorldActions
 
+  ###################
+  # Country resolvers
   def list_countries(_parent, _args, _resolution) do
     {:ok, Country |> Repo.all}
   end
@@ -17,5 +19,23 @@ defmodule IdpApiWeb.Resolvers.World do
 
   def delete_country(_parent, args, _resolution) do
     WorldActions.delete_country(args)
+  end
+
+  #################
+  # State resolvers
+  def list_states(_parent, _args, _resolution) do
+    {:ok, State |> Repo.all}
+  end
+
+  def create_state(_parent, args, _resolution) do
+    {:ok, WorldActions.create_state(args)}
+  end
+
+  def update_state(_parent, args, _resolution) do
+    WorldActions.update_state(args)
+  end
+
+  def delete_state(_parent, args, _resolution) do
+    WorldActions.delete_state(args)
   end
 end
