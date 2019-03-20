04c76b37a909a93cfffdfd624bfe549349e211a3
Sultan Iman <sultan.imanhodjaev@gmail.com>
2019-03-20 17:26:27 +0100

    config: add back guardian config and read secret key base from env

--- apps/idp_web/config/config.exs
+++ apps/idp_web/config/config.exs
@@ -12,9 +12,17 @@ config :idp_web,
 # Configures the endpoint
 config :idp_web, IdpWeb.Endpoint,
   url: [host: "localhost"],
-  secret_key_base: "1InI/+ih763K1lcIF8jX/MBGGMJE+opykgnphXF2Ozll2KQvTkZYRNXEODuu2pld",
+  secret_key_base: System.get_env("SECRET_KEY_BASE"),
   render_errors: [view: IdpWeb.ErrorView, accepts: ~w(json)],
   pubsub: [name: IdpWeb.PubSub, adapter: Phoenix.PubSub.PG2]
+
+config :idp_web, IdpWeb.Guardian,
+  issuer: "idp_web",
+  varify_module: Guardian.JWT,
+  ttl: {7, :days},
+  allow_drift: 1000,
+  verify_issuer: true,
+  secret_key: System.get_env("SECRET_KEY")
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
