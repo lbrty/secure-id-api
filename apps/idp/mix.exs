e64c7df093ec72d12a7ae56f4855e961bbec79bc
Sultan Iman <sultan.imanhodjaev@gmail.com>
2019-03-22 19:50:59 +0100

    chore: generate auth app to handle auth

--- apps/idp/mix.exs
+++ apps/idp/mix.exs
@@ -38,7 +38,9 @@ defmodule Idp.MixProject do
     [
       {:ecto_sql, "~> 3.0"},
       {:postgrex, ">= 0.0.0"},
-      {:jason, "~> 1.0"}
+      {:jason, "~> 1.0"},
+
+      {:auth, in_umbrella: true}
     ]
   end
 
