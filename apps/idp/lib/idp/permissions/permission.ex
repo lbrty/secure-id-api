881aa953f65fb526b9aea058697877665a597157
Sultan Iman <sultan.imanhodjaev@gmail.com>
2019-04-13 21:21:10 +0200

    chore: add user and project references for permissions

--- apps/idp/lib/idp/permissions/permission.ex
+++ apps/idp/lib/idp/permissions/permission.ex
@@ -1,6 +1,8 @@
 defmodule Idp.Permissions.Permission do
-  use Ecto.Schema
-  import Ecto.Changeset
+  use Idp.Model
+
+  alias Idp.Projects.Project
+  alias Idp.Users.User
 
   schema "permissions" do
     field :can_create, :boolean, default: false
@@ -10,6 +12,9 @@ defmodule Idp.Permissions.Permission do
     field :view_contacts, :boolean, default: false
     field :view_documents, :boolean, default: false
     field :view_personal, :boolean, default: false
+
+    belongs_to :user, User
+    belongs_to :project, Project
 
     timestamps()
   end
@@ -21,7 +26,9 @@ defmodule Idp.Permissions.Permission do
     :can_delete,
     :view_contacts,
     :view_personal,
-    :view_documents
+    :view_documents,
+    :user_id,
+    :project_id
   ]
 
   @doc false
