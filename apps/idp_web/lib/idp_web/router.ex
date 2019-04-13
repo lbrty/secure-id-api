c12792f69c479f1e5faaeb36e43829991240c945
Sultan Iman <sultan.imanhodjaev@gmail.com>
2019-04-13 23:13:44 +0200

    chore: use jason for absinthe

--- apps/idp_web/lib/idp_web/router.ex
+++ apps/idp_web/lib/idp_web/router.ex
@@ -6,11 +6,16 @@ defmodule IdpWeb.Router do
     plug IdpWeb.Context
   end
 
-  scope "/api", IdpWeb do
+  scope "/" do
     pipe_through :api
+
+    forward "/api", Absinthe.Plug,
+      schema: IdpWeb.Schema,
+      json_codec: Jason
 
     forward "/playground", Absinthe.Plug.GraphiQL,
       schema: IdpWeb.Schema,
+      json_codec: Jason,
       interface: :playground
   end
 end
