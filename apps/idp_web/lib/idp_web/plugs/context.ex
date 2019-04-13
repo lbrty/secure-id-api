2b75eeec5c4432f036b4b118e604b476cf964a4e
Sultan Iman <sultan.imanhodjaev@gmail.com>
2019-04-13 23:16:13 +0200

    chore: add more spaces

--- apps/idp_web/lib/idp_web/plugs/context.ex
+++ apps/idp_web/lib/idp_web/plugs/context.ex
@@ -21,7 +21,9 @@ defmodule IdpWeb.Context do
   defp build_context(conn) do
     with ["Bearer " <> token] <- get_req_header(conn, "authorization"),
          {:ok, user} <- authorize(token) do
+
       %{user: user, token: token}
+
     else
       # In case if no token found and no headers set
       # we just return empty context.
