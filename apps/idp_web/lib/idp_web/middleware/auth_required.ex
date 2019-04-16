4fbc7ef278be83606094e618e83ea80ac369d561
Sultan Iman <sultan.imanhodjaev@gmail.com>
2019-04-16 16:04:47 +0200

    feat: add middlewares to check authentication and if users are active

---
+++ apps/idp_web/lib/idp_web/middleware/auth_required.ex
@@ -0,0 +1,15 @@
+defmodule ConfettiWeb.AuthRequired do
+  @behaviour Absinthe.Middleware
+
+  use IdpWeb.Schema.Errors
+  alias Absinthe.Resolution
+
+  def call(resolution = %{context: %{user: _}}, _config) do
+    resolution
+  end
+
+  def call(resolution, _config) do
+    resolution
+    |> Resolution.put_result(@not_authenticated)
+  end
+end
---
+++ apps/idp_web/lib/idp_web/middleware/only_active_user.ex
@@ -0,0 +1,36 @@
+defmodule ConfettiWeb.OnlyActiveUser do
+  @moduledoc """
+  Middleware checks if user in the current context is
+  active before forwarding query further.
+  """
+  @behaviour Absinthe.Middleware
+
+  use IdpWeb.Schema.Errors
+  alias Absinthe.Resolution
+
+  def call(resolution = %{context: context}, _config) do
+    case Map.has_key?(context, :user) do
+      true ->
+        resolution
+        |> is_active(Map.get(context, :user))
+
+      _ ->
+        resolution
+        |> Resolution.put_result(@permission_denied)
+    end
+  end
+
+  defp is_active(resolution, %{:is_active => true}) do
+    resolution
+  end
+
+  defp is_active(resolution, %{:is_active => nil}) do
+    resolution
+    |> Resolution.put_result(@user_not_active)
+  end
+
+  defp is_active(resolution, %{:is_active => false}) do
+    resolution
+    |> Resolution.put_result(@user_not_active)
+  end
+end
