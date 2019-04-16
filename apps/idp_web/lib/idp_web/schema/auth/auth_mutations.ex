9566894a01c77c5e749ee11ccd5c6675e6954187
Sultan Iman <sultan.imanhodjaev@gmail.com>
2019-04-16 12:42:17 +0200

    chore: import project schema

--- apps/idp_web/lib/idp_web/schema/auth/auth_mutations.ex
+++ apps/idp_web/lib/idp_web/schema/auth/auth_mutations.ex
@@ -5,19 +5,19 @@ defmodule IdpWeb.Schema.AuthMutations do
   object :auth_mutations do
     @desc "Authenticate user"
     field :login, :session do
-      arg(:email, non_null(:string))
-      arg(:password, non_null(:string))
+      arg :email, non_null(:string)
+      arg :password, non_null(:string)
 
-      resolve(&AuthResolvers.login/2)
+      resolve &AuthResolvers.login/2
     end
 
     @desc "Register user"
     field :register, :text_result do
-      arg(:full_name, non_null(:string))
-      arg(:email, non_null(:string))
-      arg(:password, non_null(:string))
+      arg :full_name, non_null(:string)
+      arg :email, non_null(:string)
+      arg :password, non_null(:string)
 
-      resolve(&AuthResolvers.register/2)
+      resolve &AuthResolvers.register/2
     end
   end
 end
--- apps/idp_web/lib/idp_web/schema/schema.ex
+++ apps/idp_web/lib/idp_web/schema/schema.ex
@@ -5,12 +5,16 @@ defmodule IdpWeb.Schema do
 
   import_types(IdpWeb.Schema.TextResult)
 
+  import_types(IdpWeb.Schema.ProjectTypes)
+  import_types(IdpWeb.Schema.ProjectQueries)
+  import_types(IdpWeb.Schema.ProjectMutations)
+
   # Import authentication definitions
   import_types(IdpWeb.Schema.AuthTypes)
   import_types(IdpWeb.Schema.AuthMutations)
 
   query do
-
+    import_fields(:project_queries)
   end
 
   mutation do
