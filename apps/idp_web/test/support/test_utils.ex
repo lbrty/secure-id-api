165a0a1fee13caa6f0b3f6ba4bf8536da58b2910
Sultan Iman <sultan.imanhodjaev@gmail.com>
2019-04-16 20:16:40 +0200

    test: create test utils

---
+++ apps/idp_web/test/support/test_utils.ex
@@ -0,0 +1,13 @@
+defmodule IdpWeb.TestUtils do
+  alias Plug.Conn
+  alias Idp.Users
+
+  def get_authenticated_conn(conn, user \\ Users.get_by_email("user1@email.com"))
+  def get_authenticated_conn(conn, user) do
+    {:ok, token, _} = IdpWeb.Guardian.encode_and_sign(user)
+
+    conn
+    |> Conn.put_req_header("accept", "application/json")
+    |> Conn.put_req_header("authorization", "Bearer #{token}")
+  end
+end
