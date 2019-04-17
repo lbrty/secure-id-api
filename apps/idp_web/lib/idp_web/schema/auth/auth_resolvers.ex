87b89db463eea17bb8dce18142593f9fe8be38b6
Sultan Iman <sultan.imanhodjaev@gmail.com>
2019-04-17 22:34:26 +0200

    fix: prevent inactive users from authenticating

--- apps/idp_web/lib/idp_web/schema/auth/auth_resolvers.ex
+++ apps/idp_web/lib/idp_web/schema/auth/auth_resolvers.ex
@@ -1,12 +1,15 @@
 defmodule IdpWeb.Schema.AuthResolvers do
+  use IdpWeb.Schema.Errors
   alias Auth
 
   def login(%{email: email, password: password}, _info) do
     with {:ok, user} <- Auth.login_with_email_pass(email, password),
          {:ok, jwt, _} <- IdpWeb.Guardian.encode_and_sign(user) do
 
-      {:ok, %{token: jwt}}
-
+      case user.is_active do
+        true -> {:ok, %{token: jwt}}
+        _ -> @user_not_active
+      end
     end
   end
 
