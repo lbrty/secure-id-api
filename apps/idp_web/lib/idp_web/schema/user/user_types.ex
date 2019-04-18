f191d08ea421485ca3cde94ac59c35db283b0134
Sultan Iman <sultan.imanhodjaev@gmail.com>
2019-04-18 23:26:48 +0200

    chore: define password update input type

--- apps/idp_web/lib/idp_web/schema/user/user_types.ex
+++ apps/idp_web/lib/idp_web/schema/user/user_types.ex
@@ -5,9 +5,14 @@ defmodule IdpWeb.Schema.UserTypes do
   input_object :update_user_params do
     field(:full_name, :string)
     field(:email, :string)
-    field(:password, :string)
     field(:is_active, :boolean)
     field(:is_superuser, :boolean)
+  end
+
+  input_object :update_password_params do
+    field(:old_password, :string)
+    field(:new_password, :string)
+    field(:new_password_repeat, :string)
   end
 
   @desc "User"
