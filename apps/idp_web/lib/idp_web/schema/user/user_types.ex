d9daa55c416985c89e1e522d4f4349573394a04b
Sultan Iman <sultan.imanhodjaev@gmail.com>
2019-04-18 21:59:43 +0200

    chore: define user update input

--- apps/idp_web/lib/idp_web/schema/user/user_types.ex
+++ apps/idp_web/lib/idp_web/schema/user/user_types.ex
@@ -2,6 +2,14 @@ defmodule IdpWeb.Schema.UserTypes do
   use Absinthe.Schema.Notation
   use Absinthe.Ecto, repo: Idp.Repo
 
+  input_object :update_user_params do
+    field(:full_name, :string)
+    field(:email, :string)
+    field(:password, :password)
+    field(:is_active, :boolean)
+    field(:is_superuser, :boolean)
+  end
+
   @desc "User"
   object :user do
     field(:id, :id)
