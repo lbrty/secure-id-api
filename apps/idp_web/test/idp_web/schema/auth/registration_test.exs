9bddb63a61887737569b571e60b90a42343642af
Sultan Iman <sultan.imanhodjaev@gmail.com>
2019-04-18 21:21:26 +0200

    test: registration with short passwords is impossible

--- apps/idp_web/test/idp_web/schema/auth/registration_test.exs
+++ apps/idp_web/test/idp_web/schema/auth/registration_test.exs
@@ -41,7 +41,7 @@ defmodule IdpWeb.RegistrationSchemaTest do
       }
     end
 
-    test "can not register a new user if input is invalid", %{conn: conn} do
+    test "can not register user if input is invalid", %{conn: conn} do
       mutation = %{
         query: """
         mutation {
@@ -75,7 +75,40 @@ defmodule IdpWeb.RegistrationSchemaTest do
       }
     end
 
-    test "can not register a new user if user with same email already exists", %{conn: conn} do
+    test "can not register user if password is too short", %{conn: conn} do
+      mutation = %{
+        query: """
+        mutation {
+          register(email: "too@short.pass", password: "123", full_name: "Bla bla") {
+            result
+          }
+        }
+        """
+      }
+
+      result =
+        conn
+        |> TestUtils.get_authenticated_conn(Users.get_by_email("admin@email.com"))
+        |> post("/api/graphql", mutation)
+        |> json_response(200)
+
+      assert result == %{
+        "data" => %{"register" => nil},
+        "errors" => [
+          %{
+            "code" => "schema_errors",
+            "errors" => %{
+              "password" => "should be at least 8 character(s)"
+            },
+            "locations" => [%{"column" => 0, "line" => 2}],
+            "message" => "Changeset errors occurred",
+            "path" => ["register"]
+          }
+        ]
+      }
+    end
+
+    test "can not register user if the one with same email already exists", %{conn: conn} do
       mutation = %{
         query: """
         mutation {
