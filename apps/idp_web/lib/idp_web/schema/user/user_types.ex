1b0e147211e828b240072a2f2d8e86b6c1273968
Sultan Iman <sultan.imanhodjaev@gmail.com>
2019-04-18 22:20:57 +0200

    test: define user types

--- apps/idp_web/lib/idp_web/schema/user/user_types.ex
+++ apps/idp_web/lib/idp_web/schema/user/user_types.ex
@@ -5,7 +5,7 @@ defmodule IdpWeb.Schema.UserTypes do
   input_object :update_user_params do
     field(:full_name, :string)
     field(:email, :string)
-    field(:password, :password)
+    field(:password, :string)
     field(:is_active, :boolean)
     field(:is_superuser, :boolean)
   end
