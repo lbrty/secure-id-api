5d01a00ea71328a638fe9ce87078ee30d2cd99b9
Sultan Iman <sultan.imanhodjaev@gmail.com>
2019-04-22 23:27:12 +0200

    refactor: revert some autoformatted indents

--- apps/idp_web/test/idp_web/schema/user/user_test.exs
+++ apps/idp_web/test/idp_web/schema/user/user_test.exs
@@ -26,19 +26,19 @@ defmodule IdpWeb.UserSchemaTest do
         |> json_response(200)
 
       assert result == %{
-               "data" => %{
-                 "users" => [
-                   %{"email" => "admin@email.com", "full_name" => "Admin"},
-                   %{"email" => "user1@email.com", "full_name" => "User 1"},
-                   %{"email" => "user2@email.com", "full_name" => "User 2"},
-                   %{"email" => "user3@email.com", "full_name" => "User 3"},
-                   %{"email" => "user4@email.com", "full_name" => "User 4"},
-                   %{"email" => "user5@email.com", "full_name" => "User 5"},
-                   %{"email" => "admin2@email.com", "full_name" => "User 6"},
-                   %{"email" => "inactive@email.com", "full_name" => "Inactive user 1"}
-                 ]
-               }
-             }
+        "data" => %{
+          "users" => [
+            %{"email" => "admin@email.com", "full_name" => "Admin"},
+            %{"email" => "user1@email.com", "full_name" => "User 1"},
+            %{"email" => "user2@email.com", "full_name" => "User 2"},
+            %{"email" => "user3@email.com", "full_name" => "User 3"},
+            %{"email" => "user4@email.com", "full_name" => "User 4"},
+            %{"email" => "user5@email.com", "full_name" => "User 5"},
+            %{"email" => "admin2@email.com", "full_name" => "User 6"},
+            %{"email" => "inactive@email.com", "full_name" => "Inactive user 1"}
+          ]
+        }
+      }
     end
 
     test "admins can update any user", %{conn: conn} do
@@ -72,15 +72,15 @@ defmodule IdpWeb.UserSchemaTest do
         |> json_response(200)
 
       assert result == %{
-               "data" => %{
-                 "updateUser" => %{
-                   "email" => "user1@example.com",
-                   "full_name" => "Full name User 1",
-                   "is_active" => true,
-                   "is_superuser" => false
-                 }
-               }
-             }
+        "data" => %{
+          "updateUser" => %{
+            "email" => "user1@example.com",
+            "full_name" => "Full name User 1",
+            "is_active" => true,
+            "is_superuser" => false
+          }
+        }
+      }
     end
 
     test "admins can update passwords for other users", %{conn: conn} do
@@ -163,17 +163,17 @@ defmodule IdpWeb.UserSchemaTest do
         |> json_response(200)
 
       assert result == %{
-               "data" => %{"forceChangePassword" => nil},
-               "errors" => [
-                 %{
-                   "code" => "schema_errors",
-                   "errors" => %{"new_password_confirmation" => "Passwords do not match"},
-                   "locations" => [%{"column" => 0, "line" => 2}],
-                   "message" => "Changeset errors occurred",
-                   "path" => ["forceChangePassword"]
-                 }
-               ]
-             }
+        "data" => %{"forceChangePassword" => nil},
+        "errors" => [
+          %{
+            "code" => "schema_errors",
+            "errors" => %{"new_password_confirmation" => "Passwords do not match"},
+            "locations" => [%{"column" => 0, "line" => 2}],
+            "message" => "Changeset errors occurred",
+            "path" => ["forceChangePassword"]
+          }
+        ]
+      }
     end
 
     test "admins can see shared projects for any user", %{conn: conn} do
@@ -194,12 +194,12 @@ defmodule IdpWeb.UserSchemaTest do
         |> json_response(200)
 
       assert result == %{
-               "data" => %{
-                 "projects" => [
-                   %{"name" => "Project X"}
-                 ]
-               }
-             }
+        "data" => %{
+          "projects" => [
+            %{"name" => "Project X"}
+          ]
+        }
+      }
     end
 
     test "admins can delete any user", %{conn: conn} do
@@ -278,12 +278,12 @@ defmodule IdpWeb.UserSchemaTest do
         |> json_response(200)
 
       assert result == %{
-               "data" => %{
-                 "projects" => [
-                   %{"name" => "Project X"}
-                 ]
-               }
-             }
+        "data" => %{
+          "projects" => [
+            %{"name" => "Project X"}
+          ]
+        }
+      }
     end
 
     test "users can update their user records", %{conn: conn} do
@@ -317,18 +317,15 @@ defmodule IdpWeb.UserSchemaTest do
         |> json_response(200)
 
       assert result == %{
-               "data" => %{
-                 "updateUser" => %{
-                   "email" => "user1@example.com",
-                   "full_name" => "Full name User 1",
-                   "is_active" => true,
-                   "is_superuser" => false
-                 }
-               }
-             }
-    end
-
-    test "users can abandon shared projects", %{conn: conn} do
+        "data" => %{
+          "updateUser" => %{
+            "email" => "user1@example.com",
+            "full_name" => "Full name User 1",
+            "is_active" => true,
+            "is_superuser" => false
+          }
+        }
+      }
     end
 
     test "users can update their passwords", %{conn: conn} do
@@ -360,12 +357,12 @@ defmodule IdpWeb.UserSchemaTest do
         |> json_response(200)
 
       assert result == %{
-               "data" => %{
-                 "changePassword" => %{
-                   "email" => "user1@email.com"
-                 }
-               }
-             }
+        "data" => %{
+          "changePassword" => %{
+            "email" => "user1@email.com"
+          }
+        }
+      }
 
       # Now check if we can authenticate the user with new password
       mutation = %{
@@ -417,16 +414,16 @@ defmodule IdpWeb.UserSchemaTest do
         |> json_response(200)
 
       assert result == %{
-               "data" => %{"updateUser" => nil},
-               "errors" => [
-                 %{
-                   "code" => "permission_denied",
-                   "locations" => [%{"column" => 0, "line" => 2}],
-                   "message" => "Permission denied",
-                   "path" => ["updateUser"]
-                 }
-               ]
-             }
+        "data" => %{"updateUser" => nil},
+        "errors" => [
+          %{
+            "code" => "permission_denied",
+            "locations" => [%{"column" => 0, "line" => 2}],
+            "message" => "Permission denied",
+            "path" => ["updateUser"]
+          }
+        ]
+      }
     end
 
     test "users can not delete other user records", %{conn: conn} do
@@ -449,16 +446,16 @@ defmodule IdpWeb.UserSchemaTest do
         |> json_response(200)
 
       assert result == %{
-               "data" => %{"deleteUser" => nil},
-               "errors" => [
-                 %{
-                   "code" => "permission_denied",
-                   "locations" => [%{"column" => 0, "line" => 2}],
-                   "message" => "Permission denied",
-                   "path" => ["deleteUser"]
-                 }
-               ]
-             }
+        "data" => %{"deleteUser" => nil},
+        "errors" => [
+          %{
+            "code" => "permission_denied",
+            "locations" => [%{"column" => 0, "line" => 2}],
+            "message" => "Permission denied",
+            "path" => ["deleteUser"]
+          }
+        ]
+      }
     end
 
     test "attempt to update non existent user", %{conn: conn} do
@@ -490,16 +487,16 @@ defmodule IdpWeb.UserSchemaTest do
         |> json_response(200)
 
       assert result == %{
-               "data" => %{"updateUser" => nil},
-               "errors" => [
-                 %{
-                   "code" => "user_not_found",
-                   "locations" => [%{"column" => 0, "line" => 2}],
-                   "message" => "User not found",
-                   "path" => ["updateUser"]
-                 }
-               ]
-             }
+        "data" => %{"updateUser" => nil},
+        "errors" => [
+          %{
+            "code" => "user_not_found",
+            "locations" => [%{"column" => 0, "line" => 2}],
+            "message" => "User not found",
+            "path" => ["updateUser"]
+          }
+        ]
+      }
     end
   end
 end
