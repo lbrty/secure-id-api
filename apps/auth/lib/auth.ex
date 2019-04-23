96aaf1993768bedf5ff44ace96f3eaf52c00d940
Sultan Iman <sultan.imanhodjaev@gmail.com>
2019-04-23 18:38:32 +0200

    refactor: fix SRP violation

--- apps/auth/lib/auth.ex
+++ apps/auth/lib/auth.ex
@@ -1,36 +1,12 @@
 defmodule Auth do
   @moduledoc false
   import Comeonin.Argon2, only: [checkpw: 2]
-  alias Idp.Users
 
-  def login_with_email_pass(email, given_pass) do
-    user = Users.get_by_email(email)
-
-    cond do
-      user && checkpw(given_pass, user.password_hash) ->
-        {:ok, user}
-
-      user ->
-        {:error, :invalid_credentials}
-
-      true ->
-        {:error, :user_not_found}
-    end
-  end
-
-  def register(data) do
-    user = Users.get_by_email(data.email)
-
-    if user do
-      {:error, :already_exists}
+  def authenticate(user, given_pass) do
+    if checkpw(given_pass, user.password_hash) do
+      {:ok, :ok}
     else
-      case Users.register_user(data) do
-        {:ok, %Users.User{} = _user} ->
-          {:ok, %{result: :ok}}
-
-        error_result ->
-          error_result
-      end
+      {:error, :invalid_credentials}
     end
   end
 
