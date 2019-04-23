6a3c989485c09c087dbd085995f274a6d51fdf51
Sultan Iman <sultan.imanhodjaev@gmail.com>
2019-04-23 18:39:29 +0200

    refactor: move authentication logic into resolver

--- apps/idp_web/lib/idp_web/schema/auth/auth_resolvers.ex
+++ apps/idp_web/lib/idp_web/schema/auth/auth_resolvers.ex
@@ -2,33 +2,49 @@ defmodule IdpWeb.Schema.AuthResolvers do
   use IdpWeb.Schema.Errors
 
   alias Auth
+  alias Idp.Users
   alias Idp.EctoHelpers
 
   def login(%{email: email, password: password}, _info) do
-    with {:ok, user} <- Auth.login_with_email_pass(email, password),
+    email
+    |> Users.get_by_email()
+    |> login_user(password)
+  end
+
+  def register(%{email: email} = payload, _info) do
+    email
+    |> Users.get_by_email()
+    |> register_user(payload)
+  end
+
+  defp login_user(nil, _password), do: @user_not_found
+  defp login_user(%{is_active: false}, _password) do
+    @user_not_active
+  end
+  defp login_user(user, password) do
+    with {:ok, _} <- Auth.authenticate(user, password),
          {:ok, jwt, _} <- IdpWeb.Guardian.encode_and_sign(user) do
-      case user.is_active do
-        true -> {:ok, %{token: jwt}}
-        _ -> @user_not_active
-      end
+
+        {:ok, %{token: jwt}}
+
     else
       {:error, :invalid_credentials} -> @invalid_credentials
-      {:error, :user_not_found} -> @user_not_found
     end
   end
 
-  def register(user, _info) do
-    case Auth.register(user) do
-      {:error, :already_exists} ->
-        @user_already_exists
-
+  defp register_user(nil, payload) do
+    case Users.register_user(payload) do
       {:error, %Ecto.Changeset{} = changeset} ->
         EctoHelpers.action_wrapped(fn ->
           {:error, changeset}
         end)
 
-      {:ok, result} ->
-        {:ok, result}
+      {:ok, _result} ->
+        {:ok, %{result: :ok}}
     end
+  end
+
+  defp register_user(%Users.User{} = _user, _payload) do
+    @user_already_exists
   end
 end
