a0ac9e20cd001dfb455a99f6e411379c9ab2add1
Sultan Iman <sultan.imanhodjaev@gmail.com>
2019-04-23 18:44:43 +0200

    feat: allow admins to see permissions for any user

--- apps/idp_web/lib/idp_web/schema/permission/permission_resolvers.ex
+++ apps/idp_web/lib/idp_web/schema/permission/permission_resolvers.ex
@@ -4,7 +4,24 @@ defmodule IdpWeb.Schema.PermissionResolvers do
   alias Idp.{Permissions, Projects, Users}
   alias Idp.EctoHelpers
 
-  def list(_parent, args, %{context: %{user: _session_user}}) do
+  def list(_parent, args, _context) when args == %{} do
     {:ok, Permissions.list_permissions()}
+  end
+
+  def list(_parent, %{user_id: uid}, %{context: %{user: session_user}}) do
+    uid
+    |> Users.get_user()
+    |> list_for_user(session_user)
+  end
+
+  defp list_for_user(nil, _), do: @not_found
+  defp list_for_user(user, %{is_superuser: true}) do
+    {:ok, Permissions.list_for_user(user)}
+  end
+  defp list_for_user(user, %{id: uid}) do
+    case user.id do
+      ^uid -> {:ok, Permissions.list_for_user(user)}
+      _ -> @permission_denied
+    end
   end
 end
