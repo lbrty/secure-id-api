b82e72b48f3ecfe3d71167ca65a4d72840a015ab
Sultan Iman <sultan.imanhodjaev@gmail.com>
2019-04-24 21:18:36 +0200

    chore: de-indent deep indents from formatter

--- apps/idp_web/test/idp_web/schema/auth/registration_test.exs
+++ apps/idp_web/test/idp_web/schema/auth/registration_test.exs
@@ -25,10 +25,10 @@ defmodule IdpWeb.RegistrationSchemaTest do
         |> json_response(200)
 
       assert result == %{
-               "data" => %{
-                 "register" => %{"result" => "ok"}
-               }
-             }
+        "data" => %{
+          "register" => %{"result" => "ok"}
+        }
+      }
     end
 
     test "regular users can not register a new user", %{conn: conn} do
@@ -48,16 +48,16 @@ defmodule IdpWeb.RegistrationSchemaTest do
         |> json_response(200)
 
       assert result == %{
-               "data" => %{"register" => nil},
-               "errors" => [
-                 %{
-                   "code" => "permission_denied",
-                   "locations" => [%{"column" => 0, "line" => 2}],
-                   "message" => "Permission denied",
-                   "path" => ["register"]
-                 }
-               ]
-             }
+        "data" => %{"register" => nil},
+        "errors" => [
+          %{
+            "code" => "permission_denied",
+            "locations" => [%{"column" => 0, "line" => 2}],
+            "message" => "Permission denied",
+            "path" => ["register"]
+          }
+        ]
+      }
     end
 
     test "can not register user if input is invalid", %{conn: conn} do
@@ -78,20 +78,20 @@ defmodule IdpWeb.RegistrationSchemaTest do
         |> json_response(200)
 
       assert result == %{
-               "data" => %{"register" => nil},
-               "errors" => [
-                 %{
-                   "code" => "schema_errors",
-                   "errors" => %{
-                     "email" => "can't be blank",
-                     "password" => "should be at least 8 character(s)"
-                   },
-                   "locations" => [%{"column" => 0, "line" => 2}],
-                   "message" => "Changeset errors occurred",
-                   "path" => ["register"]
-                 }
-               ]
-             }
+        "data" => %{"register" => nil},
+        "errors" => [
+          %{
+            "code" => "schema_errors",
+            "errors" => %{
+              "email" => "can't be blank",
+              "password" => "should be at least 8 character(s)"
+            },
+            "locations" => [%{"column" => 0, "line" => 2}],
+            "message" => "Changeset errors occurred",
+            "path" => ["register"]
+          }
+        ]
+      }
     end
 
     test "can not register user if password is too short", %{conn: conn} do
@@ -112,19 +112,19 @@ defmodule IdpWeb.RegistrationSchemaTest do
         |> json_response(200)
 
       assert result == %{
-               "data" => %{"register" => nil},
-               "errors" => [
-                 %{
-                   "code" => "schema_errors",
-                   "errors" => %{
-                     "password" => "should be at least 8 character(s)"
-                   },
-                   "locations" => [%{"column" => 0, "line" => 2}],
-                   "message" => "Changeset errors occurred",
-                   "path" => ["register"]
-                 }
-               ]
-             }
+        "data" => %{"register" => nil},
+        "errors" => [
+          %{
+            "code" => "schema_errors",
+            "errors" => %{
+              "password" => "should be at least 8 character(s)"
+            },
+            "locations" => [%{"column" => 0, "line" => 2}],
+            "message" => "Changeset errors occurred",
+            "path" => ["register"]
+          }
+        ]
+      }
     end
 
     test "can not register user if the one with same email already exists", %{conn: conn} do
@@ -145,16 +145,16 @@ defmodule IdpWeb.RegistrationSchemaTest do
         |> json_response(200)
 
       assert result == %{
-               "data" => %{"register" => nil},
-               "errors" => [
-                 %{
-                   "code" => "user_already_exists",
-                   "locations" => [%{"column" => 0, "line" => 2}],
-                   "message" => "User already exists",
-                   "path" => ["register"]
-                 }
-               ]
-             }
+        "data" => %{"register" => nil},
+        "errors" => [
+          %{
+            "code" => "user_already_exists",
+            "locations" => [%{"column" => 0, "line" => 2}],
+            "message" => "User already exists",
+            "path" => ["register"]
+          }
+        ]
+      }
     end
   end
 end
