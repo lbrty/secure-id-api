b7a812ece59c0b2a0892ab733ee364d13c0bc410
Sultan Iman <sultan.imanhodjaev@gmail.com>
2019-04-25 23:08:30 +0200

    fix: remove redundant wrapping

--- apps/idp_web/lib/idp_web/schema/country/country_resolvers.ex
+++ apps/idp_web/lib/idp_web/schema/country/country_resolvers.ex
@@ -2,16 +2,12 @@ defmodule IdpWeb.Schema.CountryResolvers do
   use IdpWeb.Schema.Errors
 
   alias Idp.Geo.Countries
-  alias Idp.EctoHelpers
 
   def list(_parent, args, _context) when args == %{} do
     {:ok, Countries.list_countries()}
   end
 
   def list(_parent, %{name: name}, _context) do
-    EctoHelpers.action_wrapped(fn ->
-      name
-      |> Countries.get_by_name()
-    end)
+    {:ok, Countries.get_by_name(name)}
   end
 end
