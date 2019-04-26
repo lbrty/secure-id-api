04021ac3a84ea499cffb6ba981e83e105fcaaad7
Sultan Iman <sultan.imanhodjaev@gmail.com>
2019-04-26 22:50:48 +0200

    test: create country for admins and users

--- apps/idp_web/test/idp_web/schema/geo/countries_test.exs
+++ apps/idp_web/test/idp_web/schema/geo/countries_test.exs
@@ -1,6 +1,7 @@
 defmodule IdpWeb.CountriesSchemaTest do
   use IdpWeb.WebCase
 
+  alias Idp.Users
   alias IdpWeb.TestUtils
 
   @moduletag :countries
@@ -29,6 +30,84 @@ defmodule IdpWeb.CountriesSchemaTest do
             %{"name" => "Germany"},
             %{"name" => "Kyrgyzstan"}
           ]
+        }
+      }
+    end
+
+    test "users can not create countries", %{conn: conn} do
+      mutation = %{
+        query: """
+        mutation {
+          createCountry(country: {name: "West Britannia"}) {
+            name
+          }
+        }
+        """
+      }
+
+      result =
+        conn
+        |> TestUtils.get_authenticated_conn()
+        |> post("/api", mutation)
+        |> json_response(200)
+
+      assert result == %{
+        "data" => %{"createCountry" => nil},
+        "errors" => [
+          %{
+            "code" => "permission_denied",
+            "locations" => [%{"column" => 0, "line" => 2}],
+            "message" => "Permission denied",
+            "path" => ["createCountry"]
+          }
+        ]
+      }
+    end
+
+    test "admins can create countries", %{conn: conn} do
+      mutation = %{
+        query: """
+        mutation {
+          createCountry(country: {name: "West Britannia"}) {
+            name
+          }
+        }
+        """
+      }
+
+      result =
+        conn
+        |> TestUtils.get_authenticated_conn(Users.get_by_email("admin@email.com"))
+        |> post("/api", mutation)
+        |> json_response(200)
+
+      assert result == %{
+        "data" => %{
+          "createCountry" => %{"name" => "West Britannia"}
+        }
+      }
+    end
+
+    test "filtering for unknown country returns empty result", %{conn: conn} do
+      query = %{
+        query: """
+        {
+          countries(name: "unknown") {
+            name
+          }
+        }
+        """
+      }
+
+      result =
+        conn
+        |> TestUtils.get_authenticated_conn()
+        |> post("/api", query)
+        |> json_response(200)
+
+      assert result == %{
+        "data" => %{
+          "countries" => []
         }
       }
     end
