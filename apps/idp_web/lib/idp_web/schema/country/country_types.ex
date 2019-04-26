4b50a28ae76eece25bd73ad5edb50e64047bbe8a
Sultan Iman <sultan.imanhodjaev@gmail.com>
2019-04-26 23:16:56 +0200

    refactor: remove unused input type

--- apps/idp_web/lib/idp_web/schema/country/country_types.ex
+++ apps/idp_web/lib/idp_web/schema/country/country_types.ex
@@ -2,11 +2,6 @@ defmodule IdpWeb.Schema.CountryTypes do
   use Absinthe.Schema.Notation
   use Absinthe.Ecto, repo: Idp.Repo
 
-  @desc "Country input"
-  input_object :country_input do
-    field(:name, :string)
-  end
-
   @desc "Country"
   object :country do
     field(:name, :string)
