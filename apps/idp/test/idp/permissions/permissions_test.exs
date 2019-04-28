5c574d30a93855241ae7b9cd9772a68ec73d521e
Sultan Iman <sultan.imanhodjaev@gmail.com>
2019-04-28 17:36:18 +0200

    test: update permission context tests

--- apps/idp/test/idp/permissions/permissions_test.exs
+++ apps/idp/test/idp/permissions/permissions_test.exs
@@ -1,60 +1,37 @@
 defmodule Idp.PermissionsTest do
-  use Idp.DataCase
+  use Idp.IdpCase
 
-  alias Idp.Permissions
+  alias Idp.{Permissions, Projects, Users}
+  alias Idp.Permissions.Permission
 
-  describe "permissions" do
-    alias Idp.Permissions.Permission
+  @valid_attrs %{
+    can_create: true,
+    can_delete: true,
+    can_read: true,
+    can_update: true,
+    view_contacts: true,
+    view_documents: true,
+    view_personal: true,
+    user_id: nil,
+    project_id: nil
+  }
 
-    @valid_attrs %{
-      can_create: true,
-      can_delete: true,
-      can_read: true,
-      can_update: true,
-      view_contacts: true,
-      view_documents: true,
-      view_personal: true
-    }
-    @update_attrs %{
-      can_create: false,
-      can_delete: false,
-      can_read: false,
-      can_update: false,
-      view_contacts: false,
-      view_documents: false,
-      view_personal: false
-    }
-    @invalid_attrs %{
-      can_create: nil,
-      can_delete: nil,
-      can_read: nil,
-      can_update: nil,
-      view_contacts: nil,
-      view_documents: nil,
-      view_personal: nil
-    }
-
-    def permission_fixture(attrs \\ %{}) do
-      {:ok, permission} =
-        attrs
-        |> Enum.into(@valid_attrs)
-        |> Permissions.create_permission()
-
-      permission
-    end
-
+  describe "🚨 permissions ::" do
     test "list_permissions/0 returns all permissions" do
-      permission = permission_fixture()
-      assert Permissions.list_permissions() == [permission]
+      assert length(Permissions.list_permissions()) > 0
     end
 
-    test "get_permission!/1 returns the permission with given id" do
-      permission = permission_fixture()
-      assert Permissions.get_permission!(permission.id) == permission
+    test "get_permission/1 returns the permission with given id" do
+      permission =
+        Permissions.list_permissions()
+        |> hd()
+
+      assert Permissions.get_permission(permission.id) == permission
     end
 
     test "create_permission/1 with valid data creates a permission" do
-      assert {:ok, %Permission{} = permission} = Permissions.create_permission(@valid_attrs)
+      valid_attrs = get_valid_attrs()
+      assert {:ok, %Permission{} = permission} = Permissions.create_permission(valid_attrs)
       assert permission.can_create == true
       assert permission.can_delete == true
       assert permission.can_read == true
@@ -62,45 +39,50 @@ defmodule Idp.PermissionsTest do
       assert permission.view_contacts == true
       assert permission.view_documents == true
       assert permission.view_personal == true
+      assert permission.user_id == valid_attrs[:user_id]
+      assert permission.project_id == valid_attrs[:project_id]
     end
 
     test "create_permission/1 with invalid data returns error changeset" do
-      assert {:error, %Ecto.Changeset{}} = Permissions.create_permission(@invalid_attrs)
+      assert {:error, %Ecto.Changeset{}} = Permissions.create_permission(%{can_create: true})
     end
 
     test "update_permission/2 with valid data updates the permission" do
-      permission = permission_fixture()
+      valid_attrs = %{get_valid_attrs() | can_create: false, can_delete: false}
+      permission = Permissions.list_permissions() |> hd()
 
       assert {:ok, %Permission{} = permission} =
-               Permissions.update_permission(permission, @update_attrs)
+              Permissions.update_permission(permission, valid_attrs)
 
       assert permission.can_create == false
       assert permission.can_delete == false
-      assert permission.can_read == false
-      assert permission.can_update == false
-      assert permission.view_contacts == false
-      assert permission.view_documents == false
-      assert permission.view_personal == false
     end
 
     test "update_permission/2 with invalid data returns error changeset" do
-      permission = permission_fixture()
+      permission = Permissions.list_permissions() |> hd()
 
       assert {:error, %Ecto.Changeset{}} =
-               Permissions.update_permission(permission, @invalid_attrs)
+               Permissions.update_permission(permission, %{user_id: nil})
 
-      assert permission == Permissions.get_permission!(permission.id)
+      assert permission == Permissions.get_permission(permission.id)
     end
 
     test "delete_permission/1 deletes the permission" do
-      permission = permission_fixture()
+      permission = Permissions.list_permissions() |> hd()
       assert {:ok, %Permission{}} = Permissions.delete_permission(permission)
-      assert_raise Ecto.NoResultsError, fn -> Permissions.get_permission!(permission.id) end
+      refute Permissions.get_permission(permission.id)
     end
 
     test "change_permission/1 returns a permission changeset" do
-      permission = permission_fixture()
+      permission = Permissions.list_permissions() |> hd()
       assert %Ecto.Changeset{} = Permissions.change_permission(permission)
     end
+  end
+
+  defp get_valid_attrs do
+    user = Users.get_by_email("user1@email.com")
+    project = Projects.list_projects() |> hd()
+
+    %{@valid_attrs | user_id: user.id, project_id: project.id}
   end
 end
