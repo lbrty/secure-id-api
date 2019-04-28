513ba588ba75f293ceb595ddf2e0ebb6ce9b2eb7
Sultan Iman <sultan.imanhodjaev@gmail.com>
2019-04-28 20:09:02 +0200

    test: update tests for users context

--- apps/idp/test/idp/users/users_test.exs
+++ apps/idp/test/idp/users/users_test.exs
@@ -1,90 +1,124 @@
 defmodule Idp.UsersTest do
-  use Idp.DataCase
+  use Idp.IdpCase
 
   alias Idp.Users
+  alias Idp.Users.User
 
-  describe "users" do
+  describe "[🦄] users ::" do
     alias Idp.Users.User
 
     @valid_attrs %{
-      email: "some email",
+      email: "some@email.com",
       full_name: "some full_name",
       is_active: true,
       is_superuser: true,
+      password: "12345678",
       password_hash: "some password_hash"
     }
-    @update_attrs %{
-      email: "some updated email",
-      full_name: "some updated full_name",
-      is_active: false,
-      is_superuser: false,
-      password_hash: "some updated password_hash"
-    }
-    @invalid_attrs %{
-      email: nil,
-      full_name: nil,
-      is_active: nil,
-      is_superuser: nil,
-      password_hash: nil
-    }
-
-    def user_fixture(attrs \\ %{}) do
-      {:ok, user} =
-        attrs
-        |> Enum.into(@valid_attrs)
-        |> Users.create_user()
-
-      user
-    end
 
     test "list_users/0 returns all users" do
-      user = user_fixture()
-      assert Users.list_users() == [user]
+      assert length(Users.list_users()) > 0
     end
 
-    test "get_user!/1 returns the user with given id" do
-      user = user_fixture()
-      assert Users.get_user!(user.id) == user
+    test "get_user/1 returns the user with given id" do
+      user =
+        Users.list_users()
+        |> hd()
+
+      assert Users.get_user(user.id) == user
     end
 
-    test "create_user/1 with valid data creates a user" do
-      assert {:ok, %User{} = user} = Users.create_user(@valid_attrs)
-      assert user.email == "some email"
+    test "register_user/1 with valid data creates a user" do
+      assert {:ok, %User{} = user} = Users.register_user(@valid_attrs)
+      assert user.email == "some@email.com"
       assert user.full_name == "some full_name"
       assert user.is_active == true
       assert user.is_superuser == true
-      assert user.password_hash == "some password_hash"
+      assert user.password_hash
     end
 
-    test "create_user/1 with invalid data returns error changeset" do
-      assert {:error, %Ecto.Changeset{}} = Users.create_user(@invalid_attrs)
+    test "register_user/1 with invalid data returns error changeset" do
+      assert {:error, %Ecto.Changeset{}} = Users.register_user(%{})
     end
 
     test "update_user/2 with valid data updates the user" do
-      user = user_fixture()
-      assert {:ok, %User{} = user} = Users.update_user(user, @update_attrs)
-      assert user.email == "some updated email"
-      assert user.full_name == "some updated full_name"
-      assert user.is_active == false
-      assert user.is_superuser == false
-      assert user.password_hash == "some updated password_hash"
+      user =
+        "user1@email.com"
+        |> Users.get_by_email()
+
+      attrs = %{full_name: "xbobo"}
+      assert {:ok, %User{} = user} = Users.update_user(user, attrs)
+      assert user.full_name == "xbobo"
     end
 
     test "update_user/2 with invalid data returns error changeset" do
-      user = user_fixture()
-      assert {:error, %Ecto.Changeset{}} = Users.update_user(user, @invalid_attrs)
-      assert user == Users.get_user!(user.id)
+      user =
+        "user1@email.com"
+        |> Users.get_by_email()
+
+      assert {:error, %Ecto.Changeset{}} = Users.update_user(user, %{email: "wronk-email"})
     end
 
     test "delete_user/1 deletes the user" do
-      user = user_fixture()
+      user =
+        Users.list_users()
+        |> hd()
+
       assert {:ok, %User{}} = Users.delete_user(user)
-      assert_raise Ecto.NoResultsError, fn -> Users.get_user!(user.id) end
+      refute Users.get_user(user.id)
     end
 
     test "change_user/1 returns a user changeset" do
-      user = user_fixture()
+      user =
+        Users.list_users()
+        |> hd()
+
       assert %Ecto.Changeset{} = Users.change_user(user)
+    end
+
+    test "get_by_email/1 returns a user by email" do
+      user =
+        "user1@email.com"
+        |> Users.get_by_email()
+
+      assert user.email == "user1@email.com"
+    end
+
+    test "get_by_email/1 returns nil if the user was not found" do
+      user =
+        "xyz@email.com"
+        |> Users.get_by_email()
+
+      refute user
+    end
+
+    test "change_password/2 updates password for user" do
+      user =
+        Users.list_users()
+        |> hd()
+
+      attrs = %{
+        password: "12345678",
+        new_password: "23456781",
+        new_password_confirmation: "23456781"
+      }
+
+      {:ok, updated_user} = Users.change_password(user, attrs)
+      assert user.password_hash != updated_user.password_hash
+    end
+
+    test "force_change_password/2 updates password for user" do
+      user =
+        Users.list_users()
+        |> hd()
+
+      attrs = %{
+        new_password: "23456781",
+        new_password_confirmation: "23456781"
+      }
+
+      {:ok, updated_user} = Users.force_change_password(user, attrs)
+      assert user.password_hash != updated_user.password_hash
     end
   end
 end
